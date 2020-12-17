67f132c08c06cdcb15e7a41704fc2cd76c71c964
Brady Baker <bradybaker82@gmail.com>
2020-12-17 11:02:33 -0600

    Added reducers and comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+const searchReducer = (state, action) => {
+    // WHERE WE STORE SEARCH RESULTS TO RENDER ON DOM 
+}
+
+function* watcherSaga() {
+    // FETCH-API and make a function to FETCH from API
+    // Handle what the user searches 
+}
+
+// TODO - Function to GET API GIFS 
+
+//TODO - Function to POST what the user searches
+
+const sagaMiddleware = createSagaMiddleware();
+
+const storeInstance = createStore(
+    combineReducers({
+        searchReducer,
+    }),
+    applyMiddleware(sagaMiddleware, logger),
+);
+
+sagaMiddleware.run(watcherSaga);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('react-root'));
